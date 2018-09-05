6103f4c0f57aab8c60677c6bf8186e57ebc47417
bhardwaj.akhil96@gmail.com <akhil.bhardwaj@accionlabs.com>
2018-09-05 12:26:37 +0530

    new changes

--- frontend-backend-project/loginscreen3/src/app/app.component.ts
+++ frontend-backend-project/loginscreen3/src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   gender:String;
   adress:String;
   createdAt:Date;
+  age1:Number;
+  list=[];
+  succes1=false;
 constructor(private service : CrudService){}
 
   title = 'loginscreen3';
@@ -36,7 +39,68 @@ constructor(private service : CrudService){}
 
   public onRead(){
     console.log('Read button');
-    
+    this.service.readData(this.name).subscribe((response:any)=>{
+  console.log("Response=>",response);
+  if(response.success){
+   this.age=response.customer.age;
+    this.gender=response.customer.gender;
+    this.adress=response.customer.adress;
+    alert(response.message);
+  }else{
+    alert(response);
+    this.name=null;
+  }
+    },(err) =>{
+      alert(err.message);
+      this.name=null;
+    })
+  }
+
+  onclear(){
+this.name=null;
+this.age=null;
+this.gender=null;
+this.adress=null;
 
   }
-}
+
+  onReadall(){
+    console.log("read all daata");
+    this.service.readdataall().subscribe((response:any)=>{
+      console.log("response",response);
+      if(response.sucess){
+        this.succes1=true;
+        this.list=response.customer;
+      }
+
+    })
+  }
+    OnDelete(){
+      console.log("delete button");
+      this.service.onDelete(this.name).subscribe((response:any)=>{
+        if(response.success){
+         
+          alert(this.name+" " +"is deleted");
+          this.name=null;
+        }
+      })
+    }
+   public  onUpdate(){
+      console.log('update button');
+      const data ={
+        "age":this.age,
+        "gender":this.gender,
+        "adress":this.adress
+      }
+      this.service.updatedata(this.name, data).subscribe((response:any)=>{
+        console.log("Response=>",response);
+        if(response.success){
+          alert(response.message)
+        }else{
+          alert(response.message)
+        }
+      })
+    }
+
+
+  }
--- frontend-backend-project/loginscreen3/src/app/crud.service.ts
+++ frontend-backend-project/loginscreen3/src/app/crud.service.ts
@@ -15,7 +15,21 @@ export class CrudService {
     return this.http.post("http://localhost:3009/cust/add",{"data":data},httpOptions);
   }
   public readData(name){
-    return this.http.get('http://localhost:3009/cust/profile/${name}');
+    return this.http.get(`http://localhost:3009/cust/profile/${name}`);
+
+  }
+  public readdataall(){
+    return this.http.get("http://localhost:3009/cust/list");
+  }
+
+  public onDelete(name){
+    return this.http.delete(`http://localhost:3009/cust/delete/${name}`);
+  }
+  public updatedata(name, data){
+    const httpOptions={
+      headers:new HttpHeaders({ 'Content-Type': 'application/json'})
+    };
+    return this.http.put(`http://localhost:3009/cust/update/${name}`, {"data":data}, httpOptions);
 
   }
 }
